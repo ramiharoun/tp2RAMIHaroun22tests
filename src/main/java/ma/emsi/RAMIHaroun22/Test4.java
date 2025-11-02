eb4154f48f1fb333f145f6934c3a6543195a28f7
Rami Haroun <harounrami@outlook.com>
2025-11-03 00:12:39 +0100

    quatrieme test : la capitale de la France

--- src/main/java/ma/emsi/RAMIHaroun22/Test4.java
+++ src/main/java/ma/emsi/RAMIHaroun22/Test4.java
@@ -66,7 +66,7 @@ public class Test4 {
                         .build();
 
         // Le LLM va utiliser l'information du fichier infos.txt pour répondre à la question.
-        String question = "Pierre appelle son chat. Qu'est-ce qu'il pourrait dire ?";
+        String question = "Quelle est la capitale de la France?";
         // L'assistant recherche dans la base vectorielle les informations les plus pertinentes
         // pour répondre à la question, en comparant les embeddings de la base et celui de la question.
         // Ces informations sont ajoutées à la question et le tout est envoyé au LLM.
